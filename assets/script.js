be1a1725661c21d9439c6997b9a34be34b02e9a1
rakibulsheikh72 <158304548+rakibulsheikh72@users.noreply.github.com>
2025-11-18 04:46:27 +0600

    final commit

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,6 @@ const hamburger = document.querySelector('.hamburger');
 const mobileMenu = document.querySelector('.mobile-menu');
 
 hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');  // animate hamburger
+    hamburger.classList.toggle('active');  // animatation 
     mobileMenu.classList.toggle('active'); // show/hide menu
 });
